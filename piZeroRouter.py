3accb03fbcb60e849b608ccebbe2e49a0aee2b00
Ehab Hussein <ehabhussein@users.noreply.github.com>
2019-12-25 12:48:43 -0500

    Update piZeroRouter.py

--- piZeroRouter.py
+++ piZeroRouter.py
@@ -2,34 +2,32 @@ import binascii
 import os,sys
 import pika
 import multiprocessing
+from time import sleep
 
-fd = os.open("/dev/hidg0",os.O_RDWR)
-
-
-def mitmproxy(ip,fd):
+def mitmproxy(ip,fdR):
         try:
             qcreds2 = pika.PlainCredentials('autogfs', 'usb4ever')
             qpikaparams2 = pika.ConnectionParameters(sys.argv[1], 5672, '/', qcreds2)
             qconnect2 = pika.BlockingConnection(qpikaparams2)
             qchannel2 = qconnect2.channel()
             while True:
-                packet = binascii.hexlify(os.read(fd,64))
-                if packet:
-                    qchannel2.basic_publish(exchange='agfs', routing_key='todev',body=packet)
+                packet = fdR.read(64)
+                #if packet:
+                qchannel2.basic_publish(exchange='agfs', routing_key='todev',body=binascii.hexlify(packet))
         except Exception as e:
             print(e)
 
-
 def write2host(ch, method, properties, body):
-    print(str(binascii.unhexlify(body)))
-    os.write(fd,binascii.unhexlify(body))
+    with open("/dev/hidg0",mode='wb') as fdW:
+        print(body)
+        s = fdW.write(body)
+    print("--------------------------\n")
     ch.basic_ack(delivery_tag=method.delivery_tag)
-
 
 if __name__ == '__main__':
     try:
-        fd = os.open("/dev/hidg0",os.O_RDWR)
-        router = multiprocessing.Process(target=mitmproxy, args=(sys.argv[1],fd,))
+        fdR =  open("/dev/hidg0",mode='rb')
+        router = multiprocessing.Process(target=mitmproxy, args=(sys.argv[1],fdR))
         router.start()
         qcreds = pika.PlainCredentials('autogfs', 'usb4ever')
         qpikaparams = pika.ConnectionParameters(sys.argv[1], 5672, '/', qcreds)
@@ -41,3 +39,5 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         router.terminate()
+        #fdR.close()
+        #fdW.close()

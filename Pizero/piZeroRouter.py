28c6d673abd19c734a960bd13455507207c993b1
Ehab Hussein <ehabhussein@users.noreply.github.com>
2020-03-01 10:24:44 +0200

    Update piZeroRouter.py

--- Pizero/piZeroRouter.py
+++ Pizero/piZeroRouter.py
@@ -5,8 +5,9 @@ import os
 import threading
 import select
 import argparse
+
 try:
-    fdW = os.open("/dev/hidg0",os.O_WRONLY)
+    fdW = open("/dev/hidg0",'wb')
 except FileNotFoundError:
     print("Did you run your gadget? Guess not!\n Quitting...")
     exit(-1)
@@ -14,7 +15,7 @@ terminator = 0
 
 def makeChannel(ipaddress):
     qcreds = pika.PlainCredentials('autogfs', 'usb4ever')
-    qpikaparams = pika.ConnectionParameters(ipaddress, 5672, '/',qcreds, heartbeat=60,blocked_connection_timeout=300,socket_timeout=None)
+    qpikaparams = pika.ConnectionParameters(ipaddress, 5672, '/',qcreds)
     qconnect = pika.BlockingConnection(qpikaparams)
     return qconnect.channel()
 
@@ -28,8 +29,9 @@ def mitmProxy(ipaddress, pktlen):
                 epoll.register(hidg.fileno(), select.EPOLLIN)
                 while True:
                     if terminator == 1:
+                        print("Cleaning up!")
                         break
-                    events = epoll.poll(0.5)
+                    events = epoll.poll(0)
                     for fileno, event in events:
                         if event & select.EPOLLIN:
                             packet = hidg.read(pktlen)
@@ -43,8 +45,7 @@ def mitmProxy(ipaddress, pktlen):
 def write2host(ch, method, properties, body):
         print("VVV----------------FROM DEVICE\n")
         print(body)
-        os.write(fdW,body)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        os.write(fdW.fileno(),body)
 
 
 if __name__ == '__main__':
@@ -52,19 +53,22 @@ if __name__ == '__main__':
         argparse = argparse.ArgumentParser()
         argparse.add_argument('-ip',dest='ipaddress',help='ip address of RabbitMQ', required=True)
         argparse.add_argument('-l',dest='pktlen',help='packet length. Check bMaxPacketsize0 on your device', required=True)
+        argparse.add_argument('-s',dest='hstonly',help='Just to send packets to the host')
         args = argparse.parse_args()
         print("Go go gadget MITM!")
-        router = threading.Thread(target=mitmProxy, args=(args.ipaddress, int(args.pktlen),))
-        router.start()
+        if not args.hstonly:
+            router = threading.Thread(target=mitmProxy, args=(args.ipaddress, int(args.pktlen),))
+            router.start()
         print("Initiating Connection to RabbitMQ")
         qchannel = makeChannel(args.ipaddress)
-        qchannel.basic_qos(prefetch_count=1)
-        qchannel.basic_consume(on_message_callback=write2host, queue='tohost')
+        #qchannel.basic_qos(prefetch_count=1)
+        qchannel.basic_consume(on_message_callback=write2host, queue='tohost',auto_ack=True)
         print("Starting Consumption of queue")
         print("Started!")
         qchannel.start_consuming()
         
     except KeyboardInterrupt:
         terminator = 1
-        router.join()
+        if not args.hstonly:
+            router.join()
 

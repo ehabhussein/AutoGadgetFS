ef00432a8126a3084dea2576e147cc09c82f68f5
Ehab Hussein <ehabhussein@users.noreply.github.com>
2020-05-30 12:04:19 +0200

    Add files via upload

--- Pizero/router.py
+++ Pizero/router.py
@@ -8,7 +8,7 @@ import argparse
 from termcolor import cprint
 
 
-def showMessage(self,string,color='green',blink=None):
+def showMessage(string,color='green',blink=None):
     """shows messages if error or warn or info"""
     cprint(f"{'*'*(len(string)+4)}\n{string}\n{'*'*(len(string)+4)}",color, attrs=[] if blink is None else ['blink'])
 
@@ -21,22 +21,42 @@ except FileNotFoundError:
     showMessage("Did you run your gadget? Guess not!\n Quitting...",color="red", blink=1)
     exit(-1)
 terminator = 0
+mitmcounter = 0
 
+chksimchrNow = ""
+chksimchrPrev = ""
 
-def decodePacketAscii(payload=None):
+def decodePacketAscii(payload=None, rec=None):
     """
     This method decodes packet bytes back to Ascii
     :param payload: bytes of payload to be converted to ascii
     :return: decoded payload
     """
+    global chksimchrNow
+    global chksimchrPrev
+    if rec:
+        try:
+            chksimchrNow = payload
+        except:
+            chksimchrNow = binascii.unhexlify(binascii.hexlify(payload))
+        chksimchrForm = ""
+        for i, s in zip(chksimchrNow, chksimchrPrev):
+            try:
+                if i == s:
+                    chksimchrForm += "\u001B[44m^^\u001B[0m"
+                else:
+                    chksimchrForm += "\u001B[41m--\u001B[0m"
+            except Exception as e:
+                pass
+        chksimchrPrev = chksimchrNow
     retpayload = ""
     for i in payload:
         decode = chr(ord(chr(i)))
         if decode.isalnum():
             retpayload += decode
         else:
-            retpayload += "."
-    return retpayload
+            retpayload += " "
+    return retpayload.replace(' ', '.'), chksimchrForm if rec else ""
 
 def makeChannel(ipaddress):
     """creates a channel to RabbitMQ
@@ -56,7 +76,7 @@ def mitmProxy(ipaddress, pktlen):
     :return: None
     """
     try:
-        cprint("\t[-]Monitoring /dev/hidg0 started!",color="blue")
+        cprint("\t[-]Monitoring /dev/hidg0 started!",color="white")
         qchannel2,qconnect2 = makeChannel(ipaddress)
         epoll = select.epoll()
         epoll.register(hidg.fileno(), select.EPOLLIN)
@@ -72,6 +92,10 @@ def mitmProxy(ipaddress, pktlen):
                     packet = hidg.read(pktlen)
                     qchannel2.basic_publish(exchange='agfs', routing_key='todev', body=binascii.hexlify(packet))
             qchannel2.basic_publish(exchange='agfs', routing_key='tonull', body='')
+    except pika.exceptions.StreamLostError:
+        cprint("[+]Stream dropped re-initiating Connection to RabbitMQ",color="green")
+        cprint("\t[-]Reconnected!",color="white")
+        qchannel2,qconnect2 = makeChannel(ipaddress)    
     except Exception as e:
             print(e)
             pass
@@ -86,12 +110,16 @@ def write2host(ch, method, properties, body):
     :param body: Payload
     :return None
     """
-    cprint(f"|-[From Device]->Write packet->[To Host]{'-'*80}", color="green")
+    global mitmcounter
+    mitmcounter += 1
+    packet,diff = decodePacketAscii(payload=binascii.unhexlify(binascii.hexlify(body)),rec=1)
+    cprint(f"|-[From Device]->Write packet->[To Host][Pkt #{mitmcounter}]{'-'*80}", color="green")
     cprint(f"|\t  Bytes:", color="blue")
     cprint(f"|\t\tSent: {binascii.hexlify(body)}",color="white")
+    cprint(f"|\t\tDiff:   {diff}", color="white")
     cprint(f"|\t  Decoded:", color="blue")
-    cprint(f"|\t\t Sent: {decodePacketAscii(payload=binascii.unhexlify(binascii.hexlify(body)))}", color="white")
-    cprint(f"|{'_'*90}", color="green")
+    cprint(f"|\t\t Sent: {packet}", color="white")
+    cprint(f"|{'_'*90}[Pkt #{mitmcounter}]", color="green")
     try:
         os.write(fdW.fileno(),body)
     except Exception as e:
@@ -110,14 +138,20 @@ if __name__ == '__main__':
         if not args.hstonly:
             router = threading.Thread(target=mitmProxy, args=(args.ipaddress, int(args.pktlen),))
             router.start()
-        cprint("[+]Initiating Connection to RabbitMQ",color="blue")
+        cprint("[+]Initiating Connection to RabbitMQ",color="green")
         qchannel,qconnect = makeChannel(args.ipaddress)
        # qchannel.basic_qos(prefetch_count=1)
         qchannel.basic_consume(on_message_callback=write2host, queue='tohost',auto_ack=True)
-        cprint("\t[-]Starting Consumption of queue",color="white")
-        cprint("\t[-]Started!",color="green")
+        cprint("\t[-]Started!",color="white")
+        cprint("[+]Press Ctrl-C anytime to clean up and exit!",color="green")
+        cprint("[+]MITM session has now started!",color="blue",attrs=['blink'])
         qchannel.start_consuming()
-        
+    except pika.exceptions.StreamLostError:
+        cprint("[+]Stream dropped reinitiating Connection to RabbitMQ",color="green")
+        cprint("\t[-]Reconnected!",color="white")
+        qchannel,qconnect = makeChannel(args.ipaddress)
+        qchannel.basic_consume(on_message_callback=write2host, queue='tohost',auto_ack=True)
+        qchannel.start_consuming()
     except KeyboardInterrupt:
         terminator = 1
         qchannel.stop_consuming()
